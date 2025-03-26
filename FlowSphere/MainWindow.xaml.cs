8689e473b8052657cb5c2b59861298d918e18796
kmudhut <kamil.lepianka.03@gmail.com>
2025-03-26 01:16:48 +0100

    Initial commit. Created login view. Added logo and background svgs.

---
+++ FlowSphere/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlowSphere;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+
+    public MainWindow()
+    {
+        DataContext = this;
+        InitializeComponent();
+        HeaderText = "Logowanie";
+    }
+
+    private string _headerText;
+
+    public string HeaderText
+    {
+        get { return _headerText; }
+        set
+        {
+            _headerText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        this.DragMove();
+    }
+    private void AnimatePlaceholder(double from, double to, double opacity)
+    {
+        var anim = new DoubleAnimation(from, to, TimeSpan.FromSeconds(0.2));
+        EmailPlaceholder.RenderTransform.BeginAnimation(TranslateTransform.YProperty, anim);
+
+        var fadeAnim = new DoubleAnimation(opacity, TimeSpan.FromSeconds(0.2));
+        EmailPlaceholder.BeginAnimation(OpacityProperty, fadeAnim);
+    }
+
+    private void TextBoxInput_GotFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox || sender is PasswordBox passwordBox)
+        {
+            var elem = sender as FrameworkElement;
+
+            if (elem != null && string.IsNullOrEmpty((elem as TextBox)?.Text ?? (elem as PasswordBox)?.Password))
+            {
+                var parent = VisualTreeHelper.GetParent(elem) as Grid;
+
+                if (parent != null)
+                {
+                    foreach (var child in parent.Children)
+                    {
+                        if (child is TextBlock textBlock && child != elem)
+                        { 
+                            textBlock.RenderTransform.BeginAnimation(TranslateTransform.YProperty,
+                                new DoubleAnimation(0, -22, TimeSpan.FromSeconds(0.2)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void TextBoxInput_LostFocus(object sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox || sender is PasswordBox passwordBox)
+        {
+            var elem = sender as FrameworkElement;
+
+            if (elem != null && string.IsNullOrEmpty((elem as TextBox)?.Text ?? (elem as PasswordBox)?.Password))
+            {
+                var parent = VisualTreeHelper.GetParent(elem) as Grid;
+                
+                if (parent != null)
+                {
+                    foreach (var child in parent.Children)
+                    {
+                        if (child is TextBlock textBlock && child != elem)
+                        {
+                            textBlock.RenderTransform.BeginAnimation(TranslateTransform.YProperty,
+                                new DoubleAnimation(-22,0, TimeSpan.FromSeconds(0.2)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
