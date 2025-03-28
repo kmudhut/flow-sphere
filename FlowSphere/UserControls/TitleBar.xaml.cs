3731d18e6658922dfedb9342ca4d023e16d814c9
Kamil <kamil.lepianka.03@gmail.com>
2025-03-28 22:17:39 +0100

    Created helper function for toggling WindowState. Added new MouseLeftButtonDown handler for titlebar that implements AeroSnaps features such as DragMove on dragging maximized window.

--- FlowSphere/UserControls/TitleBar.xaml.cs
+++ FlowSphere/UserControls/TitleBar.xaml.cs
@@ -16,12 +16,13 @@ public partial class TitleBar : UserControl
         Window? parentWindow = Window.GetWindow(this);
         parentWindow?.Close();
     }
-    
+
     private void MinimizeParentWindow(object sender, RoutedEventArgs e)
     {
         Window? parentWindow = Window.GetWindow(this);
         parentWindow.WindowState = WindowState.Minimized;
     }
+
     private void MaximizeParentWindow(object sender, RoutedEventArgs e)
     {
         Window? parentWindow = Window.GetWindow(this);
@@ -35,16 +36,66 @@ public partial class TitleBar : UserControl
         }
     }
 
-    private void DragMoveParentWindow(object sender, MouseButtonEventArgs e)
+    // private void DragMoveParentWindow(object sender, MouseButtonEventArgs e)
+    // {
+    //     Window? parentWindow = Window.GetWindow(this);
+    //     parentWindow?.DragMove();
+    // }
+    //
+    // private void MaximizeParentWindowIfDoubleClicked(object sender, MouseButtonEventArgs e)
+    // {
+    //     if (e.ClickCount == 2)
+    //     {
+    //         MaximizeParentWindow(sender, e);
+    //     }
+    // }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
         Window? parentWindow = Window.GetWindow(this);
-        parentWindow?.DragMove();
-    }
-    private void MaximizeParentWindowIfDoubleClicked(object sender, MouseButtonEventArgs e)
-    {
-        if (e.ClickCount == 2) 
+        if (parentWindow == null) return;
+
+        if (e.ClickCount == 2)
         {
-            MaximizeParentWindow(sender,e);
+            ToggleMaximize();
+            return;
         }
+
+        if (parentWindow.WindowState == WindowState.Maximized)
+        {
+            Point mousePosition = PointToScreen(e.GetPosition(this));
+            
+            double previousWidth = parentWindow.RestoreBounds.Width;
+            double previousHeight = parentWindow.RestoreBounds.Height;
+            
+            double ratioX = mousePosition.X / parentWindow.ActualWidth;
+            
+            parentWindow.WindowState = WindowState.Normal;
+            
+            parentWindow.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                parentWindow.Width = previousWidth;
+                parentWindow.Height = previousHeight;
+                
+                parentWindow.Left = mousePosition.X - (parentWindow.Width * ratioX);
+                parentWindow.Top = mousePosition.Y - 10; // Małe przesunięcie w dół dla lepszego UX
+                
+                parentWindow.DragMove();
+            }));
+        }
+        else
+        {
+            parentWindow.DragMove();
+        }
+    }
+
+    private void ToggleMaximize()
+    {
+        Window? parentWindow = Window.GetWindow(this);
+        if (parentWindow == null) return;
+
+        parentWindow.WindowState = (parentWindow.WindowState == WindowState.Maximized)
+            ? WindowState.Normal
+            : WindowState.Maximized;
     }
 }
